962e5a15c8ec79a308d844065be61e3b51efdf7d
Raissa Ferreira <rai200890@gmail.com>
2016-09-01 09:10:56 -0300

    Fix field names in issue list view
    - Refactor issue list controller to use arrow functions

--- src/app/issue/issue.list.controller.js
+++ src/app/issue/issue.list.controller.js
@@ -1,12 +1,12 @@
 export default class IssuesCtrl {
   constructor(Issue) {
-    var self = this;
-    this.issues = [];
-    this.pagination = {};
-    Issue.getIssues().success(function(response, statusCode, headers){
-      self.issues = response['issues'];
-      self.pagination = response['meta'];
-    });
+      var self = this;
+      this.issues = [];
+      this.pagination = {};
+      Issue.getIssues().success((response) => {
+        self.issues = response.issues;
+        self.pagination = response.meta;
+      });
   }
 }
 
