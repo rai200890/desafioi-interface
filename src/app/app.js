34a1fb15d13328bf2e88ce37aaef2a0dd6a2458e
Raissa Ferreira <rai200890@gmail.com>
2016-09-01 09:28:13 -0300

    Display tooltip in details button from issue list

--- src/app/app.js
+++ src/app/app.js
@@ -4,11 +4,12 @@ import angular from 'angular';
 import uirouter from 'angular-ui-router';
 import routing from './app.config';
 import issue from './issue/index';
+import uibootstrap from 'angular-ui-bootstrap';
 
 const MODULE_NAME = 'app';
 const API_URL = 'http://localhost:3000';
 
-angular.module(MODULE_NAME, [uirouter, issue])
+angular.module(MODULE_NAME, [uirouter, uibootstrap, issue])
   .config(routing);
 
 export default MODULE_NAME;
