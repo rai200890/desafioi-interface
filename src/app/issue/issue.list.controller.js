94abc7413a50e7a77b143442f48ed33080cb4468
Raissa Ferreira <rai200890@gmail.com>
2016-09-02 09:14:09 -0300

    WIP: Add pagination to issue list

--- src/app/issue/issue.list.controller.js
+++ src/app/issue/issue.list.controller.js
@@ -1,13 +1,22 @@
 export default class IssuesCtrl {
   constructor(Issue) {
-      var self = this;
-      this.issues = [];
-      this.pagination = {};
-      Issue.getIssues().success((response) => {
+    var self = this;
+    self.meta = {
+      next_page: 1
+    };
+    self.issues = [];
+    self.getIssues = (tableState) => {
+      self.isLoading = true;
+      var per = 10;
+      var page = self.meta.next_page;
+      Issue.getIssues({per: 10, page: page}).success((response) => {
         self.issues = response.issues;
-        self.pagination = response.meta;
+        self.meta = response.meta;
+        tableState.pagination.numberOfPages = self.meta.total_pages;
+        self.isLoading = false;
       });
+    }
   }
 }
 
-IssuesCtrl.$inject = ['Issue'];
+  IssuesCtrl.$inject = ['Issue'];
