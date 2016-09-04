204059b1dc20a42c6a003ee8b585d2afe2b370d6
Raissa Ferreira <rai200890@gmail.com>
2016-09-04 13:14:13 -0300

    Fix pagination

--- src/app/issue/issue.list.controller.js
+++ src/app/issue/issue.list.controller.js
@@ -1,22 +1,31 @@
 export default class IssuesCtrl {
-  constructor(Issue) {
+  constructor(Issue){
     var self = this;
-    self.meta = {
-      next_page: 1
+
+    this.meta = {
+      current_page: 1
     };
-    self.issues = [];
-    self.getIssues = (tableState) => {
+
+    this.per = 15;
+    this.issues = [];
+    this.isLoading = true;
+
+    this.get = (tableState) => {
       self.isLoading = true;
-      var per = 10;
-      var page = self.meta.next_page;
-      Issue.getIssues({per: 10, page: page}).success((response) => {
+
+      var pagination = {
+        per: self.per,
+        page: self.meta.next_page
+      };
+
+      Issue.getIssues(pagination).success((response) => {
         self.issues = response.issues;
         self.meta = response.meta;
         tableState.pagination.numberOfPages = self.meta.total_pages;
         self.isLoading = false;
       });
-    }
-  }
-}
+    };
+  };
+};
 
-  IssuesCtrl.$inject = ['Issue'];
+IssuesCtrl.$inject = ['Issue'];
