b9a159b67c1d50676b0b48c90cf462c629374465
Raissa Ferreira <rai200890@gmail.com>
2016-09-04 17:57:45 -0300

    Add success and error alerts to new issue view

--- src/app/issue/issue.new.controller.js
+++ src/app/issue/issue.new.controller.js
@@ -1,10 +1,15 @@
 export default class NewIssueCtrl {
   constructor(Issue) {
     var ctrl = this;
-
     this.issue = {};
-    this.errors = [];
-
+    this.errors = null;
+    this.success = false;
+    this.closeErrors = () => {
+      ctrl.errors = null;
+    };
+    this.closeSuccess = () => {
+      ctrl.success = false;
+    };
     this.new = (issue) => {
 
     };
