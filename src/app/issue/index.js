2409dbb43be2414943476460da6b56bfa8331cfb
Raissa Ferreira <rai200890@gmail.com>
2016-09-05 01:14:40 -0300

    WIP: Add new issue form to view

--- src/app/issue/index.js
+++ src/app/issue/index.js
@@ -5,10 +5,16 @@ import routing from './issue.routes.js';
 import IssuesCtrl from './issue.list.controller.js';
 import NewIssueCtrl from './issue.new.controller.js';
 import Issue from './issue.service.js';
+import IssueType from './issue_type.service.js';
+import IssueReason from './issue_reason.service.js';
+import State from './state.service.js';
 
 export default angular.module('app.issue', [uirouter])
   .config(routing)
   .controller('IssuesCtrl', IssuesCtrl)
   .controller('NewIssueCtrl', NewIssueCtrl)
   .service('Issue', Issue)
+  .service('IssueType', IssueType)
+  .service('IssueReason', IssueReason)
+  .service('State', State)
   .name;
--- src/app/issue/issue.new.controller.js
+++ src/app/issue/issue.new.controller.js
@@ -1,19 +1,38 @@
 export default class NewIssueCtrl {
-  constructor(Issue) {
+  constructor(Issue, IssueType, IssueReason, State) {
     var ctrl = this;
     this.issue = {};
     this.errors = null;
     this.success = false;
+
+    IssueType.fetch().success((response) => {
+      ctrl.issue_types = response.issue_types;
+    });
+    IssueReason.fetch().success((response) => {
+      ctrl.issue_reasons = response.issue_reasons;
+    });
+    State.fetch().success((response) => {
+      ctrl.states = response.states;
+    });
     this.closeErrors = () => {
       ctrl.errors = null;
     };
     this.closeSuccess = () => {
       ctrl.success = false;
     };
-    this.new = (issue) => {
-
+    this.clear = () => {
+      ctrl.issue = {};
+    };
+    this.save = () => {
+      Issue.create({
+        issue: ctrl.issue
+      }).success((response) => {
+        console.info('sucesso');
+      }).error((response, status_code) => {
+        console.error('erro');
+      });
     };
   };
 };
 
-NewIssueCtrl.$inject = ['Issue'];
+NewIssueCtrl.$inject = ['Issue', 'IssueType', 'IssueReason', 'State'];
